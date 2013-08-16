de2d33148245c2bfc01a3e4e6a47362cad8fdbb2
polo90344 <polo90344@163.com>
2013-08-16 10:26:32 +0800

    加入 init.py

---
+++ chess/init.py
@@ -0,0 +1,28 @@
+import pyglet
+import Board
+import Piece
+
+
+if __name__ == "__main__":
+    window = pyglet.window.Window()
+    #背景色
+    bgcolor = (255, 255, 255, 255)
+    pyglet.gl.glClearColor(*bgcolor)
+    #创建棋盘
+    board = Board.Board()
+    #创建棋子
+    piece = Piece.Piece("象", 100, 100)
+    
+    @window.event()
+    def on_mouse_press(x, y, button, modifiers):
+        piece.Move(x, y, window)
+    
+    @window.event()
+    def on_draw():
+        window.clear()
+        board.ShowBoard()
+        piece.ShowPiece()
+        
+        
+    pyglet.app.run()
+    
